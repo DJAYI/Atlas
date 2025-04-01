8abfb4da6a5c27ec836111e8d5f69d815122b474
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-04-01 12:01:53 -0500

    Add translation for Attendance Registration in English language file

--- resources/views/assistance.blade.php
+++ resources/views/assistance.blade.php
@@ -14,11 +14,10 @@
                 @csrf
                 @method('POST')
 
-                {{-- Encabezado de formularii --}}
+                {{-- Encabezado de formulario --}}
                 <h2 class="text-3xl font-semibold">
                     {{ __('Registro de Asistencia') }}
                 </h2>
-
 
                 <div class="flex flex-col gap-3">
                     <h3 class="text-xl font-semibold text-gray-600">{{ __('Información Personal') }}</h3>
@@ -28,26 +27,30 @@
                                     class="text-secondary-400">*</span></label>
                             <input type="text" id="firstname" name="firstname"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su nombre') }}">
+                                placeholder="{{ __('Ingrese su nombre') }}"
+                                value="{{ session('found') ? session('person')->firstname : '' }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="middlename" class="text-gray-500">{{ __('Segundo Nombre') }}</label>
                             <input type="text" id="middlename" name="middlename"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su segundo nombre') }}">
+                                placeholder="{{ __('Ingrese su segundo nombre') }}"
+                                value="{{ session('found') ? session('person')->middlename : '' }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="lastname" class="text-gray-500">{{ __('Apellido') }}<span
                                     class="text-secondary-400">*</span></label>
                             <input type="text" id="lastname" name="lastname"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su apellido') }}">
+                                placeholder="{{ __('Ingrese su apellido') }}"
+                                value="{{ session('found') ? session('person')->lastname : '' }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="second_lastname" class="text-gray-500">{{ __('Segundo Apellido') }}</label>
                             <input type="text" id="second_lastname" name="second_lastname"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su segundo apellido') }}">
+                                placeholder="{{ __('Ingrese su segundo apellido') }}"
+                                value="{{ session('found') ? session('person')->second_lastname : '' }}">
                         </div>
                     </div>
                 </div>
@@ -60,20 +63,23 @@
                                     class="text-secondary-400">*</span></label>
                             <input type="email" id="email" name="email"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su correo electrónico') }}">
+                                placeholder="{{ __('Ingrese su correo electrónico') }}"
+                                value="{{ session('found') ? session('person')->email : '' }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="institutional_email"
                                 class="text-gray-500">{{ __('Correo Institucional') }}</label>
                             <input type="email" id="institutional_email" name="institutional_email"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su correo institucional') }}">
+                                placeholder="{{ __('Ingrese su correo institucional') }}"
+                                value="{{ session('found') ? session('person')->institutional_email : '' }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="phone" class="text-gray-500">{{ __('Teléfono') }}</label>
                             <input type="tel" id="phone" name="phone"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
-                                placeholder="{{ __('Ingrese su teléfono') }}">
+                                placeholder="{{ __('Ingrese su teléfono') }}"
+                                value="{{ session('found') ? session('person')->phone : '' }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="country_id" class="text-gray-500">{{ __('País de Orígen') }}<span
@@ -86,7 +92,7 @@
                                 </option>
                                 @foreach ($countries as $country)
                                     <option value="{{ $country->id }}"
-                                        {{ old('country_id', session('country_id')) == $country->id ? 'selected' : '' }}>
+                                        {{ session('found') && session('person')->country_id == $country->id ? 'selected' : '' }}>
                                         {{ $country->name }}
                                     </option>
                                 @endforeach
@@ -106,7 +112,10 @@
                                 <option value="" disabled selected>{{ __('Seleccione una universidad') }}
                                 </option>
                                 @foreach ($universities as $university)
-                                    <option value="{{ $university->id }}">{{ $university->name }}</option>
+                                    <option value="{{ $university->id }}"
+                                        {{ session('found') && session('person')->university_id == $university->id ? 'selected' : '' }}>
+                                        {{ $university->name }}
+                                    </option>
                                 @endforeach
                             </select>
                         </div>
@@ -118,7 +127,10 @@
                                 <option value="" disabled selected>{{ __('Seleccione un Programa') }}
                                 </option>
                                 @foreach ($careers as $career)
-                                    <option value="{{ $career->id }}">{{ $career->name }}</option>
+                                    <option value="{{ $career->id }}"
+                                        {{ session('found') && session('person')->career_id == $career->id ? 'selected' : '' }}>
+                                        {{ $career->name }}
+                                    </option>
                                 @endforeach
                             </select>
                         </div>
@@ -130,10 +142,18 @@
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
                                 <option value="" disabled selected>{{ __('Seleccione una opción') }}
                                 </option>
-                                <option value="estudiante">{{ __('Estudiante') }}</option>
-                                <option value="egresado">{{ __('Egresado') }}</option>
-                                <option value="profesor">{{ __('Profesor') }}</option>
-                                <option value="administrativo">{{ __('Administrativo') }}</option>
+                                <option value="estudiante"
+                                    {{ session('found') && session('person')->type == 'estudiante' ? 'selected' : '' }}>
+                                    {{ __('Estudiante') }}</option>
+                                <option value="egresado"
+                                    {{ session('found') && session('person')->type == 'egresado' ? 'selected' : '' }}>
+                                    {{ __('Egresado') }}</option>
+                                <option value="profesor"
+                                    {{ session('found') && session('person')->type == 'profesor' ? 'selected' : '' }}>
+                                    {{ __('Profesor') }}</option>
+                                <option value="administrativo"
+                                    {{ session('found') && session('person')->type == 'administrativo' ? 'selected' : '' }}>
+                                    {{ __('Administrativo') }}</option>
                             </select>
                         </div>
 
@@ -142,32 +162,56 @@
                                     class="text-secondary-400">*</span></label>
                             <select id="genre" name="genre"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
-                                <option value="M">{{ __('Masculino') }}</option>
-                                <option value="F">{{ __('Femenino') }}</option>
-                                <option value="O">{{ __('Otro') }}</option>
-                                <option value="PND">{{ __('Prefiero No Decirlo') }}</option>
+                                <option value="M"
+                                    {{ session('found') && session('person')->genre == 'M' ? 'selected' : '' }}>
+                                    {{ __('Masculino') }}</option>
+                                <option value="F"
+                                    {{ session('found') && session('person')->genre == 'F' ? 'selected' : '' }}>
+                                    {{ __('Femenino') }}</option>
+                                <option value="O"
+                                    {{ session('found') && session('person')->genre == 'O' ? 'selected' : '' }}>
+                                    {{ __('Otro') }}</option>
+                                <option value="PND"
+                                    {{ session('found') && session('person')->genre == 'PND' ? 'selected' : '' }}>
+                                    {{ __('Prefiero No Decirlo') }}</option>
                             </select>
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="birth_date" class="text-gray-500">{{ __('Fecha de Nacimiento') }}<span
                                     class="text-secondary-400">*</span></label>
                             <input type="date" id="birth_date" name="birth_date"
-                                class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
+                                class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300"
+                                value="{{ old('birth_date', session('found') ? session('person')->birth_date->format('Y-m-d') : '') }}">
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="minority" class="text-gray-500">{{ __('Pertenencia a Minoría') }}<span
                                     class="text-secondary-400">*</span></label>
                             <select id="minority" name="minority"
                                 class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
-                                <option value="">{{ __('Ninguna') }}</option>
-                                <option value="afrodescendiente">{{ __('Afrodescendiente') }}</option>
-                                <option value="indigena">{{ __('Indígena') }}</option>
-                                <option value="gitano">{{ __('Gitano') }}</option>
-                                <option value="LGTBISQ+">{{ __('LGTBISQ+') }}</option>
-                                <option value="discapacitado">{{ __('Discapacitado') }}</option>
-                                <option value="victima de conflicto armado">{{ __('Víctima de Conflicto Armado') }}
-                                </option>
-                                <option value="desplazado">{{ __('Desplazado') }}</option>
+                                <option value=""
+                                    {{ session('found') && !session('person')->minority ? 'selected' : '' }}>
+                                    {{ __('Ninguna') }}</option>
+                                <option value="afrodescendiente"
+                                    {{ session('found') && session('person')->minority == 'afrodescendiente' ? 'selected' : '' }}>
+                                    {{ __('Afrodescendiente') }}</option>
+                                <option value="indigena"
+                                    {{ session('found') && session('person')->minority == 'indigena' ? 'selected' : '' }}>
+                                    {{ __('Indígena') }}</option>
+                                <option value="gitano"
+                                    {{ session('found') && session('person')->minority == 'gitano' ? 'selected' : '' }}>
+                                    {{ __('Gitano') }}</option>
+                                <option value="LGTBISQ+"
+                                    {{ session('found') && session('person')->minority == 'LGTBISQ+' ? 'selected' : '' }}>
+                                    {{ __('LGTBISQ+') }}</option>
+                                <option value="discapacitado"
+                                    {{ session('found') && session('person')->minority == 'discapacitado' ? 'selected' : '' }}>
+                                    {{ __('Discapacitado') }}</option>
+                                <option value="victima de conflicto armado"
+                                    {{ session('found') && session('person')->minority == 'victima de conflicto armado' ? 'selected' : '' }}>
+                                    {{ __('Víctima de Conflicto Armado') }}</option>
+                                <option value="desplazado"
+                                    {{ session('found') && session('person')->minority == 'desplazado' ? 'selected' : '' }}>
+                                    {{ __('Desplazado') }}</option>
                             </select>
                         </div>
                     </div>
