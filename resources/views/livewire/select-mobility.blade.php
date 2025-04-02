8aed007ec6a76f9f18ea6e0fa726ebb1fc8f0175
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-04-02 13:31:55 -0500

    Refactor SelectMobility component view for improved readability; streamline label and select element formatting

--- resources/views/livewire/select-mobility.blade.php
+++ resources/views/livewire/select-mobility.blade.php
@@ -1,10 +1,8 @@
 <div class="flex gap-4">
     <div class="flex flex-col w-full gap-2">
-        <label for="assistanceType" class="text-gray-500">{{ __('¿Quién eres?') }}<span class="text-secondary-400">*</span></label>
-        <select 
-            wire:model.live="selectedAssistanceType" 
-            id="assistanceType" 
-            name="type"
+        <label for="assistanceType" class="text-gray-500">{{ __('¿Quién eres?') }}<span
+                class="text-secondary-400">*</span></label>
+        <select wire:model.live="selectedAssistanceType" id="assistanceType" name="type"
             class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
             <option value="" disabled selected>{{ __('Seleccione una opción') }}</option>
             <option value="estudiante">{{ __('Estudiante') }}</option>
@@ -15,11 +13,9 @@
     </div>
 
     <div class="flex flex-col w-full gap-2">
-        <label for="mobility" class="text-gray-500">{{ __('¿A qué vienes?') }}<span class="text-secondary-400">*</span></label>
-        <select 
-            id="mobility" 
-            wire:model.live="mobility" 
-            name="mobility"
+        <label for="mobility" class="text-gray-500">{{ __('¿A qué vienes?') }}<span
+                class="text-secondary-400">*</span></label>
+        <select id="mobility" wire:model.live="mobility" name="mobility"
             class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
             <option value="" disabled selected>{{ __('Seleccione una opción') }}</option>
             @foreach ($mobilities as $mob)
@@ -27,4 +23,4 @@
             @endforeach
         </select>
     </div>
-</div>s
+</div>
