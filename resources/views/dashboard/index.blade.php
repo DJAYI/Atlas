a4ca96d6db768e2b24dfda2916a91e929846023f
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-04-09 14:06:21 -0500

    Refactor dashboard report section; adjust headings for consistency and improve layout styling.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,10 +5,11 @@
         </h2>
     </div>
     <div class="flex flex-col gap-4 mt-4 h-full">
-        <div class="grid grid-cols-[1fr_20px_1fr]  mt-20 items-center gap-8">
+        <h2 class="text-xl font-semibold my-3 text-primary-700 px-2">Reportes de Movilidad</h2>
+        <div class="grid grid-cols-[1fr_20px_1fr] p-8 border rounded-md items-center gap-8">
             <div class="grid grid-cols-1 col-span-1 self-start gap-4 px-3 relative">
 
-                <h2 class="text-2xl font-semibold my-3 text-secondary-400 px-2 text-center">Reportes de Movilidad Foránea
+                <h2 class="text-lg font-semibold my-3 text-secondary-400 px-2 text-center">Reportes de Movilidad Foránea
                 </h2>
 
                 @php
@@ -25,13 +26,12 @@
             </div>
 
             <div class="flex-col col-span-1 items-center justify-center hidden md:flex">
-                <hr class="h-56 w-[3px] bg-gradient-to-t from-primary-300 to-transparent">
-                <hr class="h-56 w-[3px] bg-gradient-to-b from-secondary-300 to-transparent">
+                <hr class="min-h-96 w-[1px] bg-gray-300/60">
             </div>
 
             <div class="grid grid-cols-1 col-span-1 self-start gap-4 px-3 relative">
 
-                <h2 class="text-2xl text-center font-semibold my-3 text-secondary-400 px-2">Reportes de Movilidad Local
+                <h2 class="text-lg text-center font-semibold my-3 text-secondary-400 px-2">Reportes de Movilidad Local
                 </h2>
 
                 @php
