636d2428ebd8f10c5b61b4cad9fac45bf773132b
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-04-11 12:30:51 -0500

    Update language files and labels for improved clarity; add new translations and modify existing labels for consistency.

--- resources/views/livewire/select-mobility.blade.php
+++ resources/views/livewire/select-mobility.blade.php
@@ -13,7 +13,7 @@
     </div>
 
     <div class="flex flex-col w-full gap-2 col-span-1">
-        <label for="mobility" class="text-gray-500">{{ __('¿A qué vienes?') }}<span
+        <label for="mobility" class="text-gray-500">{{ __('Propósito de Asistencia') }}<span
                 class="text-secondary-400">*</span></label>
         <select id="mobility" wire:model.live="mobility" name="mobility_id"
             class="w-full px-4 py-2 transition bg-white border rounded-lg shadow-sm border-primary-300">
