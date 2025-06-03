6865a4db56b5928652d7f7d741f9a47782a787f4
DJAYI <programador7@tecnocomfenalco.edu.co>
2025-06-03 10:42:50 -0500

    feat: Implement complete attendance report table with data structure

---
+++ app/Livewire/Charts/TableTotalAssistances.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Charts;
+
+use Livewire\Component;
+
+class TableTotalAssistances extends Component
+{
+    public function render()
+    {
+        return view('livewire.charts.table-total-assistances');
+    }
+}
