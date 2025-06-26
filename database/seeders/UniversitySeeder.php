5c6a49afa00ad8ba62a8a6bc78375588c980138b
DJAYI <danilo.arenasyi@gmail.com>
2025-06-26 12:15:10 -0500

    fix: Comment out factory creation and ensure specific university entry in UniversitySeeder

--- database/seeders/UniversitySeeder.php
+++ database/seeders/UniversitySeeder.php
@@ -9,6 +9,11 @@ class UniversitySeeder extends Seeder
 {
     public function run(): void
     {
-        University::factory(rand(8, 10))->create();
+        // University::factory(rand(8, 10))->create();
+        University::create([
+            'name' => 'Fundación Universitaria Tecnológico Comfenalco',
+            'code' => 'FUTC',
+            'description' => 'Fundación Universitaria Tecnológico Comfenalco es una institución de educación superior ubicada en Colombia, enfocada en ofrecer programas académicos de alta calidad y formación integral a sus estudiantes.',
+        ]);
     }
 }
